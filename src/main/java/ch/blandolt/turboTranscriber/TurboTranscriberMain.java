ffebc3c6f3e0b8450f2b9524d9d1528b634243fc
blandolt <balduin.landolt@unibas.ch>
2019-05-03 01:04:44 +0200

    feature: maven product structure; feature: main class (stub)

---
+++ src/main/java/ch/blandolt/turboTranscriber/TurboTranscriberMain.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package ch.blandolt.turboTranscriber;
+
+/**
+ * @author Balduin Landolt
+ *
+ */
+public class TurboTranscriberMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hello World");
+	}
+
+}
