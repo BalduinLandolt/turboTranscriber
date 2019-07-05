720c332d781030cdfd1b74143482bbe182406f53
BalduinLandolt <balduin.landolt@unibas.ch>
2019-07-05 19:11:17 +0200

    Feature (Tokenization): overwrite toString()

--- src/main/java/ch/blandolt/turboTranscriber/util/datastructure/tokenization/TranscriptionToken.java
+++ src/main/java/ch/blandolt/turboTranscriber/util/datastructure/tokenization/TranscriptionToken.java
@@ -9,4 +9,9 @@ public class TranscriptionToken implements Tokenizable {
     public String getText() {
         return text;
     }
+
+    public String toString(){
+        String s = super.toString();
+        return s + ": " + text;
+    }
 }
