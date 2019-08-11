a0796b53e292821cf8fa0f08323b83fb77e7141b
BalduinLandolt <balduin.landolt@unibas.ch>
2019-08-11 09:14:43 +0200

    Feature (XML): Add processing instructions

--- src/main/java/ch/blandolt/turboTranscriber/util/datastructure/XMLFactory.java
+++ src/main/java/ch/blandolt/turboTranscriber/util/datastructure/XMLFactory.java
@@ -3,6 +3,7 @@ package ch.blandolt.turboTranscriber.util.datastructure;
 import ch.blandolt.turboTranscriber.util.datastructure.nativeRepresentation.AbstractTranscriptionObject;
 import org.jdom2.Document;
 import org.jdom2.Element;
+import org.jdom2.ProcessingInstruction;
 
 import java.util.List;
 
@@ -11,6 +12,17 @@ public class XMLFactory {
     public static Document createXML(List<AbstractTranscriptionObject> data) {
         Element root = new Element("TEI", "http://www.tei-c.org/ns/1.0");
         Document res = new Document(root);
+        ProcessingInstruction relaxng = new ProcessingInstruction("xml-model",
+                "href='http://www.tei-c.org/release/xml/tei/custom/schema/relaxng/tei_all.rng' " +
+                        "type='application/xml' schematypens='http://relaxng.org/ns/structure/1.0'");
+        res.addContent(0, relaxng);
+        ProcessingInstruction schematron = new ProcessingInstruction("xml-model",
+                "href='http://www.tei-c.org/release/xml/tei/custom/schema/relaxng/tei_all.rng' " +
+                        "type='application/xml' schematypens='http://purl.oclc.org/dsdl/schematron'");
+        res.addContent(1, schematron);
+        ProcessingInstruction stylesheet = new ProcessingInstruction("xml-stylesheet",
+                "href='style.css' type='text/css'");
+        res.addContent(2, stylesheet);
 
         // TODO implement adding data
 
