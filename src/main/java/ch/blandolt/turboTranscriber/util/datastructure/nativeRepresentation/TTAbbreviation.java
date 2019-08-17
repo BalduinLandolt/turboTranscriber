b3371d8446860460e6e2b6d692a2ccd3000f4570
BalduinLandolt <balduin.landolt@unibas.ch>
2019-08-17 22:54:25 +0200

    Feature: Add TODO

--- src/main/java/ch/blandolt/turboTranscriber/util/datastructure/nativeRepresentation/TTAbbreviation.java
+++ src/main/java/ch/blandolt/turboTranscriber/util/datastructure/nativeRepresentation/TTAbbreviation.java
@@ -15,7 +15,8 @@ public class TTAbbreviation extends AbstractTranscriptionContainer {
     }
 
     public TTAbbreviation(TranscriptionToken ex, TranscriptionToken infix, TranscriptionToken am) {
-        super(new LinkedList<AbstractTranscriptionObject>(Arrays.asList(
+        // TODO: fix. logic of what is atomar and what not, seems messed up
+        this(new LinkedList<AbstractTranscriptionObject>(Arrays.asList(
                 new TTExpansion(new TTPlainTextSegment(ex.getText())),
                 new TTTextSegment(new TTPlainTextSegment(infix.getText())),
                 new TTAbbreviationMark(TTAbbreviationMark.convertTokenClassSpecific(am)))));
