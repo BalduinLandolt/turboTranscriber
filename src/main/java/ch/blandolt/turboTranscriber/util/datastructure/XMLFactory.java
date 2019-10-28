fbee69eff7506fcc9a4c9d074a03e55d0a234d55
blandolt <balduin.landolt@unibas.ch>
2019-10-28 13:20:37 +0100

    Add TODO

--- src/main/java/ch/blandolt/turboTranscriber/util/datastructure/XMLFactory.java
+++ src/main/java/ch/blandolt/turboTranscriber/util/datastructure/XMLFactory.java
@@ -12,6 +12,8 @@ public class XMLFactory {
 
     // TODO: ensure that tags with multiple arguments work properly
 
+    // FIXME: name tags do't wrap around words anymore
+
 
     public static Document createTEIXML(List<AbstractTranscriptionObject> data) {
         Element root = new Element("TEI", ns_TEI);
