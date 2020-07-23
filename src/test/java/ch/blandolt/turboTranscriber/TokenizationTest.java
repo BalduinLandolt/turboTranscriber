d29a66a32ae01ce27ad83f28c3bea9264f2d193a
BalduinLandolt <balduin.landolt@unibas.ch>
2020-07-23 23:25:17 +0200

    Test: slightly improve tokenization test

--- src/test/java/ch/blandolt/turboTranscriber/TokenizationTest.java
+++ src/test/java/ch/blandolt/turboTranscriber/TokenizationTest.java
@@ -34,11 +34,15 @@ public class TokenizationTest {
      */
     @Test
     public void testTokenizationBasics() {
+        String text;
+        TTRDocument doc;
+        long start;
+        List<TranscriptionToken> tokens;
 
         // empty text document
-        String text = "bla bla";
-        TTRDocument doc = new TTRDocument(new TextDocumentItem("uri", "ttr", 0, text));
-        long start = System.currentTimeMillis();
+        text = "";
+        doc = new TTRDocument(new TextDocumentItem("uri", "ttr", 0, text));
+        start = System.currentTimeMillis();
         while (!doc.isTokenized()) {
             try {
                 Thread.sleep(10);
@@ -49,8 +53,116 @@ public class TokenizationTest {
         }
         log.info("Finished waiting for tokens after (" + Long.valueOf(System.currentTimeMillis()-start) + ")ms");
         assertTrue(doc.isTokenized());
-        List<TranscriptionToken> tokens = doc.getTokens();
+        tokens = doc.getTokens();
+        assertNotNull(tokens);
+        assertEquals(0, tokens.size());
+
+        // simple words
+        text = "bla bla";
+        doc = new TTRDocument(new TextDocumentItem("uri", "ttr", 0, text));
+        start = System.currentTimeMillis();
+        while (!doc.isTokenized()) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        log.info("Finished waiting for tokens after (" + Long.valueOf(System.currentTimeMillis()-start) + ")ms");
+        assertTrue(doc.isTokenized());
+        tokens = doc.getTokens();
         assertNotNull(tokens);
         assertEquals(2, tokens.size());
+
+        // glyph
+        text = "{slong}";
+        doc = new TTRDocument(new TextDocumentItem("uri", "ttr", 0, text));
+        start = System.currentTimeMillis();
+        while (!doc.isTokenized()) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        log.info("Finished waiting for tokens after (" + Long.valueOf(System.currentTimeMillis()-start) + ")ms");
+        assertTrue(doc.isTokenized());
+        tokens = doc.getTokens();
+        assertNotNull(tokens);
+        assertEquals(1, tokens.size());
+
+        // word with glyph
+        text = "{slong}em";
+        doc = new TTRDocument(new TextDocumentItem("uri", "ttr", 0, text));
+        start = System.currentTimeMillis();
+        while (!doc.isTokenized()) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        log.info("Finished waiting for tokens after (" + Long.valueOf(System.currentTimeMillis()-start) + ")ms");
+        assertTrue(doc.isTokenized());
+        tokens = doc.getTokens();
+        assertNotNull(tokens);
+        assertEquals(1, tokens.size());
+
+        // abbreviation
+        text = "(m;bar)";
+        doc = new TTRDocument(new TextDocumentItem("uri", "ttr", 0, text));
+        start = System.currentTimeMillis();
+        while (!doc.isTokenized()) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        log.info("Finished waiting for tokens after (" + Long.valueOf(System.currentTimeMillis()-start) + ")ms");
+        assertTrue(doc.isTokenized());
+        tokens = doc.getTokens();
+        assertNotNull(tokens);
+        assertEquals(1, tokens.size());
+
+        // word with glyph and abbreviation
+        text = "{slong}e(m;bar)";
+        doc = new TTRDocument(new TextDocumentItem("uri", "ttr", 0, text));
+        start = System.currentTimeMillis();
+        while (!doc.isTokenized()) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        log.info("Finished waiting for tokens after (" + Long.valueOf(System.currentTimeMillis()-start) + ")ms");
+        assertTrue(doc.isTokenized());
+        tokens = doc.getTokens();
+        assertNotNull(tokens);
+        assertEquals(1, tokens.size());
+
+        // tags
+        text = "[div]\n[lb=2] sem \n[/div]";  // FIXME: if no space after "sem", test will fail
+        doc = new TTRDocument(new TextDocumentItem("uri", "ttr", 0, text));
+        start = System.currentTimeMillis();
+        while (!doc.isTokenized()) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        log.info("Finished waiting for tokens after (" + Long.valueOf(System.currentTimeMillis()-start) + ")ms");
+        assertTrue(doc.isTokenized());
+        tokens = doc.getTokens();
+        assertNotNull(tokens);
+        assertEquals(4, tokens.size());
     }
 }
